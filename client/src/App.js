26c0b9b8e2077f003f6309d311929f59e8f041d2
JedwardMook <jedwardmook@yahoo.com>
2022-12-12 14:14:29 -0600

    removes unneeded dependencies, fixes hook call problem, adds first scss files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import LogIn from './LogIn';
 import MapContainer from './MapContainer';
 import SignUp from './SignUp';
@@ -6,11 +7,19 @@ import SignUp from './SignUp';
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <MapContainer />
-      <SignUp />
-      <LogIn />
+      <Routes>
+      <Route path='signup'
+        element={<SignUp />}
+        />
+      <Route path='login'
+        element={<LogIn />}
+        />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,3 +1,4 @@
+import './Styles/signup.min.css';
 import React, { useState } from "react";
 
 function SignUp() {
