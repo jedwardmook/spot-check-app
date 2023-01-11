7d05f469365d5174e980f7bfa5493586107451ae
JedwardMook <jedwardmook@yahoo.com>
2023-01-11 10:36:52 -0600

    Fleshes out spot.js with styling and address logic using react-geocode, adds react-geocode

--- client/src/Components/Spot.js
+++ client/src/Components/Spot.js
@@ -1,8 +1,22 @@
+import '../Styles/spot.min.css';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import Geocode from 'react-geocode'
+import { useParams, Link } from 'react-router-dom'
 
 function Spot() {
     const [displaySpot, setDisplaySpot] = useState()
+    const [address, setAddress] = useState()
+
+    Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+    Geocode.setLocationType("ROOFTOP");
+    displaySpot && Geocode.fromLatLng(displaySpot.lat, displaySpot.lng).then((response) => {
+        const number = response.results[0].address_components[1].long_name;
+        const street = response.results[0].address_components[2].long_name;
+        const city = response.results[0].address_components[4].long_name;
+        const state = response.results[0].address_components[6].long_name;
+        const zipcode = response.results[0].address_components[8].long_name;
+        setAddress(`${number} ${street}. ${city}, ${state} ${zipcode}`)
+    })
 
     let params = useParams()
     let displayId = params.spotId
@@ -17,10 +31,22 @@ function Spot() {
         })
         }, [])
 
-    console.log(displaySpot)
 
   return (
-    <div>Spot</div>
+    displaySpot ? 
+        <div className="display_spot_div">
+            <div className="display_spot_links">
+                <Link to="/spots/add_review"><h5 className="header_link">Add Review</h5></Link>
+                <h5 className="header_link">Edit Spot</h5>
+            </div>
+            <div className="display_spot_photos_div">
+            </div>
+            <div className="display_spot_info_div">
+                <p className="display_spot_name">{displaySpot.name}</p>
+                <p className="display_spot_address">{address}</p>
+                <p className="display_spot_about">{displaySpot.about}</p>
+            </div>
+        </div> : <div>Spot Loading</div>
   )
 }
 
